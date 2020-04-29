64e7a009f6d5c00a272294dd431063688ed34b12
Benno Selig <selig.benno@gmail.com>
2020-04-29 18:09:43 +0200

    feat(config): configure rollup and babel for react and typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,24 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
 import pkg from "./package.json";
 
+const extensions = [".ts, .tsx"];
+const plugins = [
+  resolve(),
+  commonjs(),
+  babel({
+    exclude: "node_modules/**",
+    extensions,
+  }),
+  typescript(),
+];
+
 export default {
-  input: "src/index.ts",
-  // plugins:
+  input: "src/index.tsx",
+  plugins,
   output: [
     {
       file: pkg.main,
