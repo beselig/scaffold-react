751be76717ca3088d6dad860d4bee3d2a4b25a7b
Benno Selig <selig.benno@gmail.com>
2020-05-12 07:18:53 +0200

    chore(bundling): change fuse.ts config, clean up package.json

--- fuse.ts
+++ fuse.ts
@@ -1,50 +1,54 @@
 import { fusebox, sparky } from "fuse-box";
 
-const bundlePath = 'dist/app.js'
+const bundlePath = "dist/app.js";
 
 class Context {
-  runServer?: boolean;
-  sourceMap?: boolean;
-  getConfig = () =>
-    fusebox({
-      target: "browser",
-      entry: "src/index.tsx",
-      cache: false,
-      devServer: this.runServer,
-      webIndex: {
-        template: "src/index.html",
-      },
-      sourceMap: this.sourceMap
-    });
+    runServer?: boolean;
+    sourceMap?: boolean;
+    getConfig = () =>
+        fusebox({
+            target: "browser",
+            entry: "src/index.tsx",
+            cache: false,
+            devServer: this.runServer,
+            webIndex: {
+                template: "src/index.html",
+            },
+            sourceMap: this.sourceMap,
+            dependencies: {
+                include: ["tslib"],
+			},
+			hmr: true
+        });
 }
 
 const { task } = sparky<Context>(Context);
 
 task("default", async (ctx) => {
-  ctx.runServer = true;
-  ctx.sourceMap = true;
-  const fuse = ctx.getConfig();
-  await fuse.runDev({
-    uglify: false,
-    bundles: { distRoot: "public", app: bundlePath, vendor: "dist/vendor.js" },
-  });
+    ctx.runServer = true;
+    ctx.sourceMap = true;
+    const fuse = ctx.getConfig();
+    await fuse.runDev({
+        uglify: false,
+        bundles: { distRoot: "public", app: bundlePath, vendor: "dist/vendor.js" },
+    });
 });
 
 task("preview", async (ctx) => {
-  ctx.runServer = true;
-  ctx.sourceMap = true;
-  const fuse = ctx.getConfig();
-  await fuse.runProd({
-    uglify: false,
-    bundles: { distRoot: "public", app: bundlePath, vendor: "dist/vendor.js" },
-  });
+    ctx.runServer = true;
+    ctx.sourceMap = true;
+    const fuse = ctx.getConfig();
+    await fuse.runProd({
+        uglify: false,
+        bundles: { distRoot: "public", app: bundlePath, vendor: "dist/vendor.js" },
+    });
 });
 
 task("dist", async (ctx) => {
-  ctx.runServer = false;
-  const fuse = ctx.getConfig();
-  await fuse.runProd({
-    uglify: true,
-    bundles: { distRoot: "public", app: bundlePath, vendor: "dist/vendor.js" },
-  });
+    ctx.runServer = false;
+    const fuse = ctx.getConfig();
+    await fuse.runProd({
+        uglify: true,
+        bundles: { distRoot: "public", app: bundlePath, vendor: "dist/vendor.js" },
+    });
 });
