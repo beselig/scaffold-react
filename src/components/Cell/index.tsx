357ca8644580c3b0d6338e3eff612642ce993246
Benno Selig <selig.benno@gmail.com>
2020-05-13 18:32:01 +0200

    feat(Cell): Add support for pencilmarks to Cell component

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,16 +1,30 @@
 import React from "react";
-import "./component.css";
+import "./component.scss";
 
 type CellProps = {
-  id: string;
-  value: string;
-  invalid?: boolean;
-  selected?: boolean;
-  pencilmarks?: string[];
+    id: string;
+    value: string;
+    invalid?: boolean;
+    selected?: boolean;
+    pencilmarks?: string[];
 };
 
-export const Cell = ({ id, value, ...props }: CellProps): JSX.Element => (
-  <div className="cell" id={id}>
-    {value}
-  </div>
+export const Cell = ({
+    id,
+    value,
+    pencilmarks,
+}: CellProps): JSX.Element => (
+    <div className="cell" id={id}>
+        {value ? (
+            <p className="value">{value}</p>
+        ) : pencilmarks ? (
+            <div className="pencilmarks">
+                {pencilmarks.map((str) => (
+                    <span>
+                        <p>{str}</p>
+                    </span>
+                ))}
+            </div>
+        ) : null}
+    </div>
 );
